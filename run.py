94157f3624136309f442953f83e18a16b78e7385
AyyItsAljo <aljaz.medic@gmail.com>
2019-04-25 19:13:11 +0200

    Commit made available run from run.sh, added build.sh for building

--- run.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-from device_registry import app as device_registry_app
-
-device_registry_app.run(host='0.0.0.0', port=80, use_reloader=True, threaded=True, debug=True)
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+docker-compose up -d
+echo "Running in background"
+docker-compose ps | echo
